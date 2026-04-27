cf0a074851963f31f5744083afa5392edbdf9f05
Bayological <6872903+bayological@users.noreply.github.com>
2026-04-27 11:43:07 -0500

    chore: add mainnet address registries

--- src/core/constants/borrowRegistries.ts
+++ src/core/constants/borrowRegistries.ts
@@ -7,12 +7,13 @@ import { ChainId } from './chainId'
 export const borrowRegistries: Record<number, Record<string, string>> = {
   [ChainId.CELO]: {
     GBPm: '0xB3136DBadB14Ab587FFa91545538126938Fe0C6E',
-    // TODO(@nvtaveras): Add the mapping from the token symbol to address registry on CELO here, just like Sepolia
+    CHFm: '0xCa70801D91576d069190d1D4CFDDEbdc237A4537',
+    JPYm: '0x8f99Aac2FE09A1390617D4AcDD1519f775eE931A',
   },
   [ChainId.CELO_SEPOLIA]: {
     GBPm: '0x8b33D626E8d79388889d404fBC515Ed131c9508e',
     CHFm: '0x1e3CCCC62cEBd9Bf2a26Ba512E3abee086816c58',
-    JPYm: '0x812e5ccec5e55b81f4270898fdf1c916e3a284fb',
+    JPYm: '0x812e5ccEC5E55B81F4270898FDF1C916e3a284Fb',
   },
 }
 
